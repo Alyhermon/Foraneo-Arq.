3257879519fbdcea8c1ae88d44c70248ea9e6192
alyhermon <Alinahermon11@gmail.com>
2022-11-21 02:17:08 -0400

    Comenzamos con Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Inicio', component: HomePageComponent},
+  {path: 'About', component: AboutComponent},
+  {path: 'Contactos', component: ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
